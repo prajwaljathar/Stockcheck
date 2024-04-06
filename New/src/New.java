b7eeb3be97e1c4bb0725ef22df2285e029371c0c
PrajwalJathar <62589799+prajwaljathar@users.noreply.github.com>
2024-04-06 22:18:58 +0530

    Signed-off-by: admin <prajwaljathar1999@gmail.com>

--- New/src/New.java
+++ New/src/New.java
@@ -7,6 +7,7 @@ public class New {
 		int [] array= {1,4,5,9,7,8,10};
 		int[] sortedArray=Arrays.stream(array).sorted().toArray();
 		System.out.println(Arrays.toString(sortedArray));
+		int [] arrafgy= {1,4,5,9,7,8,10};
 	}
 
 
