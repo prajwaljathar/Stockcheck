30b4f8c782011c8c27a684ce6d0212445f972869
PrajwalJathar <62589799+prajwaljathar@users.noreply.github.com>
2024-04-06 21:56:32 +0530

    Add files via upload

---
+++ New/src/New.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class New {
+	
+	public static void main(String[] args){
+		
+		int [] array= {1,4,5,9,7,8,10};
+		int[] sortedArray=Arrays.stream(array).sorted().toArray();
+		System.out.println(Arrays.toString(sortedArray));
+	}
+
+
+}
